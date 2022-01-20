3e28fe88ae7cca8281075f1d4a480b3c24559926
Antoine Do Nascimento <antoinedn@live.fr>
2022-01-20 18:12:55 +0100

    readd get from api

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from API!" });
 });
 
 app.listen(port, '0.0.0.0', function(err) {
